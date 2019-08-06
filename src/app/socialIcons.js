656f346b211eaafd363a5cecdf5b5c0e79e0451d
Timothy McDonald <tim.mcdonald@deepend.melbourne>
2019-08-06 10:23:56 +1000

    add new jobs

--- src/app/socialIcons.js
+++ src/app/socialIcons.js
@@ -8,7 +8,7 @@ export class SocialIcons extends Component {
 				<li><a className="social-icons--icon icon-twitter" href="http://www.twitter.com/timmkd" target="_blank"><span>Twitter</span></a></li>
 				<li><a className="social-icons--icon icon-linkedin" href="https://uk.linkedin.com/in/tim-mcdonald-2232a531" target="_blank"><span>Linkedin</span></a></li>
 				<li><a className="social-icons--icon icon-mail" href="mailto:tim.mkdonald@gmail.com" target="_blank"><span>Email</span></a></li>
-				<li><span className="social-icons--icon icon-mobile"><span>Phone</span></span> 0770 3800 574</li>
+				<li><span className="social-icons--icon icon-mobile"><span>Phone</span></span> 0450 124 714</li>
 			</ul>
 		);
 	}
